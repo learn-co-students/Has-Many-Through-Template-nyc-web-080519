331fc88aea8bce119fb31a2ba3f1fa4a9a23c630
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-08-11 10:11:28 -0400

    made models for magazine, person, subscription

--- lib/Model1.rb
+++ lib/Model1.rb
@@ -1,5 +1,48 @@
+require 'pry'
 #class for Model1 goes here
 #Feel free to change the name of the class
-class Model1
+class Person
 
-end
+  attr_accessor :name, :address
+
+  @@all = []
+
+  def initialize(name, address)
+    @name = name
+    @address = address
+    @@all << self  #can also write as self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  #create a method that joins this class to the other through the joiner
+  def subscribe_to_magazine(magazine)
+    Subscription.new(self, magazine)
+  end
+
+  def my_magazines
+    magazines = []
+    Subscription.all.each do |subscription|
+      magazines << subscription.magazine if subscription.person == self
+    end
+    magazines
+  end
+
+  def total_magazines
+    my_magazines.length
+  end
+
+end #end person class
+
+#make some Magazine and Person instances seed data then call the subscribe to connedt this person to a magazine
+
+
+# When creating the models, be sure to include rich instance variables, 
+# so you can do more with the models! For instance, a doctor model might have things 
+# like name, field (field of study), and cost while a patient model might have things 
+# like name, ailments and income. In doing this, you might want to write methods 
+# where a patient can find the doctors (s)he can afford and/or a doctor appropriate 
+# for his/her pain.
+
--- lib/Model2.rb
+++ lib/Model2.rb
@@ -1,5 +1,39 @@
+require 'pry'
 #class for Model2 goes here
 #Feel free to change the name of the class
-class Model2
+class Magazine
 
-end
+  attr_accessor :title, :num_issues
+
+  @@all = []
+
+  def initialize(title, num_issues=12)
+    @title = title
+    @num_issues = num_issues
+    @@all << self  #can also write as self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+   #create a method that joins this class to the other through the joiner
+   def add_subscriber(person)
+    Subscription.new(person, self)
+  end
+
+  def my_subscribers
+    subscribers = []
+    Subscription.all.each do |subscription|
+      subscribers << subscription.person if subscription.magazine == self
+    end
+    subscribers
+  end
+
+  def total_subscribers
+    my_subscribers.length
+  end
+
+end #end Magazine class
+
+#make some Magazine and Person instances seed data then call the add_subscriber to connedt this magazine to a person
